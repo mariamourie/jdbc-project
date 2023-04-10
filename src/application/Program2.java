12d24a853981519aedfe05255ce31afb360aa053
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2023-04-10 18:32:30 -0300

    Department: insert implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 1: department insert ===");
+		Department department = new Department(null, "Cosmetics");
+		departmentDao.insert(department);
+		System.out.println("Inserted! New Id = " + department.getId());
+		
+		sc.close();
+
+	}
+
+}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -88,7 +88,7 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;	
 		try {
 			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
-			
+			 
 			st.setInt(1, id);
 			
 			st.executeUpdate();
